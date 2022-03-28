eb803461780c0666ae27ea1d1493d2ba27bb3d5c
mayra.rossi03@gmail.com <mayra.rossi03@gmail.com>
2022-03-28 10:53:10 -0300

    orden

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+/*****ENIGMA GAME******/
+
+//Colores para el código
 const colors = ["blue","green","yellow","black","red","brown","violet","pink","orange","yellow"];
       
-
+//Usuario
 class User {
     constructor(nickname){
         this.id;
@@ -8,6 +11,7 @@ class User {
     }
 }
 
+//Clase madre de los códigos 
 class Codes {
     constructor(type,columns){
         this.id;
@@ -16,46 +20,30 @@ class Codes {
     }
 }
 
+//Clase creadora del codigo a descifrar
 class TheCode extends Codes {
     constructor(type,columns){
         super(type,columns);
         this.code = [];
-
     }
     generateCode(){
-      
-      console.log(this.type);
-      console.log(this.columns);
-      console.log(typeof(this.columns));
-      
       for(var i=0;i<this.columns;i++){
-        console.log("ingresa al for")
-        
-        var number = Math.random();
-      number = number*9;
-      number = Math.floor(number);
-      
-      console.log(number);
-      
-      if (this.type == "Colors"){
+       var number = Math.random();
+       number = number*9;
+       number = Math.floor(number);
+       if (this.type == "Colors"){
         this.code[i] = colors[number]
-        console.log(this.code);
-      } else{
+       } else{
         this.code[i]=number;
         console.log(this.code[i])
-        }
+       }
+      }
+      console.log(this.code);
+      guessing(this.code,this.type);
     }
-    console.log(this.code);
-    console.log(this.code.length);
-    
-    guessing(this.code,this.type);
-  
-
-    
-    }
-
 }
 
+//Clase creadora de respuestas de usuarios
 class Guessings extends Codes {
     constructor(type,columns){
         super(type,columns);
@@ -64,6 +52,8 @@ class Guessings extends Codes {
 
 }
 
+
+//Clase que da respuesta a los intentos del usuario
 class Clues {
     constructor(){
         this.id;
@@ -74,12 +64,32 @@ class Clues {
 }
 
 
+
+//Función que da inicio a cada juego
+//El usuario selecciona los detalles del código antes de iniciar (Si es con números o con colores y si se va a utilizar un código de 3,4,5,6 o 7 espacios)
 function start(){
-  var select = document.getElementById('selectOptions').innerHTML=' <h3>Para comenzar seleccioná las siguientes opciones</h3><h4>Elementos del código</h4><select name="tope" id="tope"><option value="2">Números</option><option value="1">Colores</option></select><h4>Cantidad de espacios del código</h4><select name="columns" id="columns"><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option></select><br><br><button type="submit" onclick="createNewCode(tope.value,columns.value)">Generar código</button> ';
+  var select = document.getElementById('selectOptions').innerHTML=`  
+  <h3>Para comenzar seleccioná las siguientes opciones</h3>
+  <h4>Elementos del código</h4>
+  <select name="tope" id="tope">
+   <option value="2">Números</option>
+   <option value="1">Colores</option>
+  </select>
+  <h4>Cantidad de espacios del código</h4>
+  <select name="columns" id="columns">
+   <option value="3">3</option>
+   <option value="4">4</option>
+   <option value="5">5</option>
+   <option value="6">6</option>
+   <option value="7">7</option>
+  </select>
+  <br>
+  <br>
+  <button type="submit" onclick="createNewCode(tope.value,columns.value)">Generar código</button> `;
 }
 
 
-
+// Se crea un código aleatorio automático
 function createNewCode (t,c){
   console.log(t)
   
@@ -99,7 +109,10 @@ function guessing(code,type){
   var l =code.length;
   var t = type=="Colors"?"color":"numero";
   console.log('llega'+l)
-  document.getElementById('selectOptions').innerHTML='<h3> Código generado</h3><h4>Ahora intenta adivinar el código</h4><div id="answering"></div>';
+  document.getElementById('selectOptions').innerHTML= `<h3>Código generado</h3>
+   <h4>Ahora intenta adivinar el código</h4>
+   <div id="answering"></div>
+  `;
   
   inputs(l,t);
     
@@ -110,8 +123,11 @@ function inputs(l,t){
   
 var inputs =[];
   for (var j = 0; j < l; j++) {
-    inputs.push('<h4>Indique el '+t+' '+(j+1)+ '</h4><input type="text">')
-     }
+    inputs.push(`
+     <h4>Indique el ${t} ${j+1}</h4>
+     <input type="text">
+    `)
+  }
   document.getElementById('answering').innerHTML=inputs;
     
      
